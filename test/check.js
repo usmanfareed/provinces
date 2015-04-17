bc5626987d9bc334b143d8e566c6c19e2008ba9b
James Halliday <mail@substack.net>
2015-04-17 05:06:42 -0700

    failing duplicate test

--- test/check.js
+++ test/check.js
@@ -9,3 +9,16 @@ test('all fields present', function (t) {
         t.ok(has(p, 'country'));
     });
 });
+
+test('no duplicates', function (t) {
+    var seen = {};
+    t.plan(provinces.length);
+    provinces.forEach(function (p) {
+        if (!seen[p.country]) seen[p.country] = {};
+        t.ok(
+            !has(seen[p.country], p.name),
+            'duplicate ' + p.country + ': ' + p.name
+        );
+        seen[p.country][p.name] = true;
+    });
+});
